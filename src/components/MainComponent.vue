f3932ecb0fa5d7ed90dc739e8ebbd6cd98488892
valentinaiori <valentinaiori@live.it>
2022-09-23 11:39:31 +0200

    Add improvements

--- src/components/MainComponent.vue
+++ src/components/MainComponent.vue
@@ -10,7 +10,7 @@
 
 <script>
 export default {
-    name:'SectionMainComponent'
+    name:'MainComponent'
 
 }
 </script>
--- src/components/ShopAreaComponent.vue
+++ src/components/ShopAreaComponent.vue
@@ -27,7 +27,7 @@
 
 <script>
 export default {
-    name: 'SecondSectionComponent'
+    name: 'ShopAreaComponent'
 }
 </script>
 
